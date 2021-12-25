7daee35f9758ab3e352717a02ae1b169469964d9
GSuganya24 <96653473+GSuganya24@users.noreply.github.com>
2021-12-25 18:48:34 +0530

    Add files via upload
    week1.day1_Assignment

---
+++ OddNumbers.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class OddNumbers {
+	public static void main(String[] args) {
+		System.out.println("Printing Only Odd Numbers from 1 to 20");
+		int a = 20;
+		for (int i=1;i<a;i++) {
+			if(i%2==1) {
+				System.out.println("Odd Number : " +i);
+				}
+		}
+			
+	}
+
+}
